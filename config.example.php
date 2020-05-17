9376d09274123f30fa2843edb1d5d2a90fb4e2f2
Gleb <zvgleb@yandex.ru>
2020-05-17 23:30:20 +0300

    hash

--- config.example.php
+++ config.example.php
@@ -5,6 +5,7 @@ return [
         "user" => "root",
         "pass" => "zzzzgggg",
         "host" => "localhost",
+        "hash" => '$2y$10$8.VUQJx8TzCzjsioRLysT.X8tHKOlW8ye4tao92lpZ8JgBjoIhjSG'
     ],
 
 ];
